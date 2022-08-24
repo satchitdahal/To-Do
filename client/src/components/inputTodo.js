93059e2437ed37e1fd9cb64e62c4b164c87cb018
satchitdahal <84872532+satchitdahal@users.noreply.github.com>
2022-08-24 13:17:44 -0700

    Final toDO update with refresher after adding toDo

--- client/src/components/inputTodo.js
+++ client/src/components/inputTodo.js
@@ -1,55 +1,37 @@
+import e from "cors";
 import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
 
+    //this is a react hook 
+    //the description is the current state 
+    //the setDescription is a method that will be used to update the state
+    //whatever is in the "" in the useState, will be the default value 
+    //to be displayed 
     const [description, setDescription] = useState("")
 
-
-    return <Fragment>
-        <h1 className="text-center mt-5 ">Input Todo</h1>
-        <form className="d-flex" mt-5>
-            <input type="text" className="form-control" value={description} />
-
     const onSubmitForm = async (e) => {
-                e.preventDefault()
+        e.preventDefault()
         try {
-            const body = {description}
+            const body = { description }
             //by default fetch makes a get request
-            const response = await fetch("http://localhost:5000/todos", {
+            const response = fetch("http://localhost:5000/todos", {
                 method: "POST",
-            //what is this?
-            headers: {"Content-Type": "application/json" },
-            body: JSON.stringify(body)
                 //what is this?
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(body)
             })
+            //refreshes after the Submit 
+            window.location = "/"
         } catch (err) {
             console.error(err.message)
 
         }
     }
 
-            return <Fragment>
-                <h1 className="text-center mt-5 ">Input Todo</h1>
-                {/* after making the onSubmit method, attach it to this form */}
-                <form className="d-flex" mt-5 onSubmit={onSubmitForm}>
-
-                    <input
-                        type="text"
-                        className="form-control"
-                        //we are setting the value of the input to the description
-                        //note that the description is the current state
-                        //this cannot be changed unless we have an onChange method
-                        value={description}
-                        //now the onChange will call the setDescription method 
-                        //which will target the input and get the value
-                        //e is the event
-                        onChange={e => setDescription(e.target.value)} />
-                    {/* this button is attached to this form
     return <Fragment>
         <h1 className="text-center mt-5 ">Input Todo</h1>
-        <form className="d-flex" mt-5>
+        <form className="d-flex" mt-5 onSubmit={onSubmitForm}>
 
             <input
                 type="text"
@@ -62,6 +44,8 @@ const InputTodo = () => {
                 //which will target the input and get the value
                 //e is the event
                 onChange={e => setDescription(e.target.value)} />
+            {/*add button is attached to form so it will submit the form
+                when pressed  */}
             <button className="btn btn-success">Add</button>
         </form>
     </Fragment >
