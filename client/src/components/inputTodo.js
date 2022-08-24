063a5de922739905a35fda5bbead454c7d767da7
satchitdahal <84872532+satchitdahal@users.noreply.github.com>
2022-08-24 12:53:44 -0700

    need to work on learning what fetch does
    the inputTodo component is good

--- client/src/components/inputTodo.js
+++ client/src/components/inputTodo.js
@@ -1,13 +1,47 @@
+import e from "cors";
 import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
 
+    //this is a react hook 
+    //the description is the current state 
+    //the setDescription is a method that will be used to update the state
+    //whatever is in the "" in the useState, will be the default value 
+    //to be displayed 
     const [description, setDescription] = useState("")
+
+    const onSubmitForm = async (e) => {
+        e.preventDefault()
+        try {
+            const body = { description }
+            //by default fetch makes a get request
+            const response = fetch("http://localhost:5000/todos", {
+                method: "POST",
+                //what is this?
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            })
+        } catch (err) {
+            console.error(err.message)
+
+        }
+    }
 
     return <Fragment>
         <h1 className="text-center mt-5 ">Input Todo</h1>
         <form className="d-flex" mt-5>
-            <input type="text" className="form-control" value={description} />
+
+            <input
+                type="text"
+                className="form-control"
+                //we are setting the value of the input to the description
+                //note that the description is the current state
+                //this cannot be changed unless we have an onChange method
+                value={description}
+                //now the onChange will call the setDescription method 
+                //which will target the input and get the value
+                //e is the event
+                onChange={e => setDescription(e.target.value)} />
             <button className="btn btn-success">Add</button>
         </form>
     </Fragment >
