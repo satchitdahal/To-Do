a0fb62b0de7ec36de8676cf91a82bb20b835dc98
satchitdahal <84872532+satchitdahal@users.noreply.github.com>
2022-08-24 13:07:28 -0700

    Update inputTodo.js

--- client/src/components/inputTodo.js
+++ client/src/components/inputTodo.js
@@ -4,14 +4,51 @@ const InputTodo = () => {
 
     const [description, setDescription] = useState("")
 
+
     return <Fragment>
         <h1 className="text-center mt-5 ">Input Todo</h1>
         <form className="d-flex" mt-5>
             <input type="text" className="form-control" value={description} />
-            <button className="btn btn-success">Add</button>
-        </form>
-    </Fragment >
+
+    const onSubmitForm = async (e) => {
+                e.preventDefault()
+        try {
+            const body = {description}
+            //by default fetch makes a get request
+            const response = await fetch("http://localhost:5000/todos", {
+                method: "POST",
+            //what is this?
+            headers: {"Content-Type": "application/json" },
+            body: JSON.stringify(body)
+            })
+        } catch (err) {
+                console.error(err.message)
+
+            }
+    }
+
+            return <Fragment>
+                <h1 className="text-center mt-5 ">Input Todo</h1>
+                {/* after making the onSubmit method, attach it to this form */}
+                <form className="d-flex" mt-5 onSubmit={onSubmitForm}>
+
+                    <input
+                        type="text"
+                        className="form-control"
+                        //we are setting the value of the input to the description
+                        //note that the description is the current state
+                        //this cannot be changed unless we have an onChange method
+                        value={description}
+                        //now the onChange will call the setDescription method 
+                        //which will target the input and get the value
+                        //e is the event
+                        onChange={e => setDescription(e.target.value)} />
+                    {/* this button is attached to this form
+            so thats why when you click on the button, the form is submitted */}
+                    <button className="btn btn-success">Add</button>
+                </form>
+            </Fragment >
 
 };
 
-export default InputTodo;
+            export default InputTodo;
