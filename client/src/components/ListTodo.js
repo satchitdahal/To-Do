343b242f155c7fb5c80efa92172b3c8822083b10
satchitdahal <84872532+satchitdahal@users.noreply.github.com>
2022-08-25 11:46:43 -0700

    delete works
    need to comment and learn react hook useEffect

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,12 +1,38 @@
 import e from "cors";
 import React, { Fragment, useEffect, useState } from "react";
 
+
 const ListTodos = () => {
 
+    const [todos, setTodos] = useState([])
 
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+
+            })
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+    const getTodo = async () => {
+        try {
+
+            const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json()
+
+            setTodos(jsonData)
+        } catch (err) {
+            console.error(err.message)
+
+        }
+    }
     useEffect(() => {
-
-    })
+        getTodo()
+    }, [])
     return (
         <Fragment>
             {" "}
@@ -19,16 +45,21 @@ const ListTodos = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>john@example.com</td>
-                    </tr>
+                    {todos.map(todo => (
+
+                        <tr key={todo.todo_id}>
+                            <td>{todo.description}</td>
+                            <td><button className="btn btn-edit">Edit</button></td>
+                            <td><button className="btn btn-danger " onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
+                        </tr>
+
+                    ))}
+
                 </tbody>
             </table>
 
 
-        </Fragment>
+        </Fragment >
     )
 }
 
